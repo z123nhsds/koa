1cd0d19077571207423aedb15f3ad820e566d735
z <2129147720@qq.com>
2026-06-04 05:48:26 +0000

    1

--- __tests__/response/type.test.js
+++ __tests__/response/type.test.js
@@ -49,29 +49,7 @@ describe('ctx.type=', () => {
       assert(!ctx.response.header['content-type'])
     })
   })
-})
-
 describe('ctx.type', () => {
   describe('with no Content-Type', () => {
     it('should return ""', () => {
       const ctx = context()
-      assert(!ctx.type)
-    })
-  })
-
-  describe('with a Content-Type', () => {
-    it('should return the mime', () => {
-      const ctx = context()
-      ctx.type = 'json'
-      assert.strictEqual(ctx.type, 'application/json')
-    })
-  })
-
-  describe('when setting to +json content type', () => {
-    it('should set the content type to json', () => {
-      const ctx = context()
-      ctx.type = 'application/vnd.myapi.v1+json'
-      assert.strictEqual(ctx.type, 'application/vnd.myapi.v1+json')
-    })
-  })
-})
